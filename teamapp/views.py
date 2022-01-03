249cfdd76363a82747c241896876f7047dfe88d1
s1f10210360 <85855502+s1f10210360@users.noreply.github.com>
2022-01-03 17:08:27 +0900

    make app

--- teamapp/views.py
+++ teamapp/views.py
@@ -1,3 +1,86 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.http import Http404
+from django.utils import timezone
+import random
+from teamapp.models import Article,Comment
 
-# Create your views here.
+def index(request):
+	if request.method == 'POST':
+		article = Article(title=request.POST['title'],body=request.POST['text'])
+		article.save()
+		return redirect(detail, article.id)
+
+	if ('sort' in request.GET):
+		if request.GET['sort']=='like':
+			articles=Article.objects.order_by('-like')
+		else:
+			articles=Article.objects.order_by('-posted_at')
+	else:
+		articles=Article.objects.order_by('-posted_at')			
+		
+	context = {
+		"articles":articles
+    }
+	return render(request, 'teamapp/index.html', context)
+
+def hello(request):
+	data={
+		'name':'Alice',
+		'account': True,
+		'menu':['自分の投稿: なし', '保存済み: 0', 'フォロー中: 0', 'フォロワー: 0'],
+		'news':'ありません'
+	}
+	return render(request, 'teamapp/hello.html', data)
+
+def redirect_test(request):
+	return redirect(hello)	
+
+def detail(request, article_id):
+	try:
+		article= Article.objects.get(pk=article_id)
+	except Article.DoesNotExist:
+		raise Http404("Article does not exist")
+	if request.method=='POST':
+		comment=Comment(article=article, text=request.POST['text'])
+		comment.save()
+
+	context={
+		'article': article,
+		'comments':article.comments.order_by('-posted_at')
+	}	
+	return render(request, "teamapp/detail.html",context)
+
+def update(request, article_id):
+	try:
+		article=Article.objects.get(pk=article_id)
+	except Article.DoesNotExist:
+		raise Http404("Article does not exist")	
+	if request.method =='POST':
+		article.title=request.POST['title']
+		article.body=request.POST['text']
+		article.save()
+		return redirect(detail, article_id)		
+	context={
+		"article":article
+	}	
+	return render(request, "teamapp/edit.html",context)
+
+def delete(request, article_id):
+	try:
+		article=Article.objects.get(pk=article_id)
+	except Article.DoesNotExist:
+		raise Http404("Article does not exist")
+
+	article.delete()		
+
+	return redirect(index)
+
+def like(request, article_id):
+	try:
+		article=Article.objects.get(pk=article_id)
+		article.like += 1
+		article.save()
+	except Article.DoesNotExist:
+		raise Http404("Article does not exist")
+	return redirect(detail,article_id)	
